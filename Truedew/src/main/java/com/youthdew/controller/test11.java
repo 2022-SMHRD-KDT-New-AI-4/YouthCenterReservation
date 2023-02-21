bad9038e460e175f1c323295ea6905c0f62193fd
smhrd <smhrd@121.147.0.159>
2023-02-21 15:31:44 +0900

    커밋

---
+++ Truedew/src/main/java/com/youthdew/controller/test11.java
@@ -0,0 +1,9 @@
+package com.youthdew.controller;
+
+public class test11 {
+
+	public static void main(String[] args) {
+		System.out.println("a");
+	}
+
+}
---
+++ Truedew/src/main/java/com/youthdew/db/SqlSessionManager.java
@@ -0,0 +1,29 @@
+package com.youthdew.db;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class SqlSessionManager {
+
+	private static SqlSessionFactory sqlSessionFactory;
+	
+	static {
+		try {
+			String resource = "com/youthdew/db/config.xml";
+			InputStream inputStream;
+			inputStream = Resources.getResourceAsStream(resource);
+			sqlSessionFactory =
+					new SqlSessionFactoryBuilder().build(inputStream);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static SqlSessionFactory getSqlSession() {
+		return sqlSessionFactory;
+	}
+}
